8d43da4c242d2c5a98ba9116819f488935a7d2f4
arthur lelieur <arthur.lelieur2@gmail.com>
2025-10-10 10:43:18 +0200

    petit formulaire avec méthode POST

--- index.php
+++ index.php
@@ -1,29 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Initiation à PHP</title>
+        <title></title>
     </head>
     <body>
-        <h1>PHP</h1>
-        <div>
-            <?php
-                $tabledebut = $_GET["tabledebut"];
-                $tablefin = $_GET["tablefin"];
-                $i = 0;
+        <h1>Saisir les informations</h1>
 
-                for($j = $tabledebut; $j <= $tablefin; $j++)
-                {
-                    while($i <= 10)
-                    {
-                        echo $i . " x " . $j . " = " . $i * $j . "<br>";
-                        $i++;
-                    }
-                    $i = 0;
-                    echo "<br>";
-                }
-            ?>
-        </div>
+        <form action="traitement.php" method="POST">
+
+            <p>Prénom <input type="text" name="prenom" required></p>
+            <p>Nom <input type="text" name="nom" required></p>
+
+            <input type="submit" value="Valider">
+        </form>
     </body>
 </html>
---
+++ traitement.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>
+            <?php
+                echo "Bonjour " .$_POST['prenom']. " ".$_POST['nom'];
+            ?>
+        </p>
+    </body>
+</html>
