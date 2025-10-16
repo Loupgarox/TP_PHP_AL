db6c1e7f317ba957526aac9ef63fa2bebb0db9fb
arthur lelieur <arthur.lelieur2@gmail.com>
2025-10-16 11:20:08 +0200

    rajout de la proposition de l'idfilm max +1

--- formulaireInsert.php
+++ formulaireInsert.php
@@ -10,9 +10,17 @@ and open the template in the editor.
         <title></title>
     </head>
         <h1>Ajouter un nouveau film</h1>
+        <?php 
+            include "connexion.php";
+
+            $requete = "select max(id) from film";
+            $commande = Connexion::getInstance()->prepare($requete);
+            $commande->execute();
+            $maxId = $commande->fetch();
+        ?>
         <form action="insertFilm.php" method="post">
             Identifiant du film :
-            <input type="number" name="idfilm" required>
+            <input type="number" name="idfilm" value=<?php echo $maxId[0] + 1 ?> required>
             <br>
             Identifiant du réalisateur :
             <input type="number" name="idrealisateur" required>
