073b8729b5436df6451c2327de80068f14610d67
arthur lelieur <arthur.lelieur2@gmail.com>
2025-10-17 08:35:46 +0200

    Idrealisateur affiche un seul Id dans la combo box

--- formulaireInsert.php
+++ formulaireInsert.php
@@ -17,13 +17,20 @@ and open the template in the editor.
             $commande = Connexion::getInstance()->prepare($requete);
             $commande->execute();
             $maxId = $commande->fetch();
+
+            $requete2 = "select nom from realisateur";
+            $commande = Connexion::getInstance()->prepare($requete2);
+            $commande->execute();
+            $nomRealisateur = $commande->fetch();
         ?>
         <form action="insertFilm.php" method="post">
             Identifiant du film :
             <input type="number" name="idfilm" value=<?php echo $maxId[0] + 1 ?> required>
             <br>
             Identifiant du réalisateur :
-            <input type="number" name="idrealisateur" required>
+            <select>
+                <option value = "idrealisateur"><?php echo $nomRealisateur[0] ?></option>
+            </select>
             <br>
             Titre :
             <input type="text" name="titre" required>
